08e71c19334ad543ff0a012337536532b77587cf
Vincent Calabro <vinny@1stdibs.com>
2013-11-08 02:01:11 -0500

    sample config file

---
+++ setup/configuration.sample.php
@@ -0,0 +1,6 @@
+<?php   define('USER', '');         // email address
+        define('PASSWORD', '');     // email password
+
+        define('DB_USER', 'root');
+        define('DB_HOST', 'localhost');
+        define('DB_PASSWORD', '');
