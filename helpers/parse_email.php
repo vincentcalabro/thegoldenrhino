10ad361f1c7de4682a8b6e2bc828f8d0a57c6a31
Vincent Calabro <vinny@1stdibs.com>
2013-11-08 02:01:27 -0500

    spiff this email thing up a bit, create an Email class. currently not able to pop messages into an array for some reason

--- helpers/parse_email.php
+++ helpers/parse_email.php
@@ -1,97 +1,70 @@
 <?php
+require_once('/Users/vinny/projects/thegoldenrhino/setup/configuration.php');
 
-$server = "{imap.gmail.com:993/ssl}";
-$mailbox = "[Gmail]/Chats";
-$needle = "chat with jesse goodenough";
-$user = "";             //email address
-$password = "";         //password
+class Email {
+    private $server = "{imap.gmail.com:993/ssl}";
+    private $mailbox = "[Gmail]/Chats";
+    private $imap;
+    private $db;
 
-$imap = imap_open($server.$mailbox, $user, $password);
+    public function __construct($user, $password) {
+        $this->user = $user;
+        $this->password = $password;
+        $this->imap = imap_open($this->server.$this->mailbox, $this->user, $this->password);  // get imap connection
+    }
 
-//$message_count = imap_num_msg($imap);
-//echo "Total message count: ".$message_count."<br />";
+    public function connectDB() {
+        $db = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD);
+        return $db;
+    }
 
-function getMailboxNames($imap, $server, $mailbox) {
-    $mailboxes = imap_list($imap, $server, '*');
-    foreach($mailboxes as $mailbox) {
-        $shortname = str_replace($server, '', $mailbox);
-        echo "$shortname\n";
+    public function getMailboxNames() {
+        $imap = $this->imap;
+        $server = $this->server;
+
+        $mailboxes = imap_list($imap, $server, '*');
+        foreach($mailboxes as $mailbox) {
+            $shortname = str_replace($server, '', $mailbox);
+            echo "$shortname\n";
+        }
+    }
+
+    public function getChatsFromJesse() {
+        $imap = $this->imap;
+        $chats = imap_search($imap, 'ALL', SE_UID);
+        $chats_arr = array();
+
+        $needle = "chat with jesse goodenough";     // search for this subject
+        $to_blank = array("display:block","padding-left:6em","text-indent:-1em",";");   // styles to replace with empty string
+
+        foreach($chats as $value) {
+            $headers = imap_headerinfo($imap, $value);
+            if(isset($headers->subject) && stristr(strtolower($headers->subject), $needle) == true) {
+                $body = imap_fetchbody($imap, $value, "2");
+                $body = str_replace($to_blank,"",$body);
+                $body = preg_replace("/(0?\d|1[0-2]):(0\d|[0-5]\d) (AM|PM)/i","<span style='color:#777;'>$1:$2 $3</span>",$body);
+                $body = "<div class='conversation'>".$body."</div>";
+                array_push($chats_arr, $body);
+            }
+        }
+        return $chats_arr;
+    }
+
+    public function insertChats() {
+        $db = $this->connectDB();
+        $chats_arr = $this->getChatsFromJesse();
+        foreach($chats_arr as $value) {
+            $sql = "insert into email.conversations (body) values ('".$value."')";
+            mysql_query($sql,$db);
+        }
+    }
+
+    public function closeImapConnection() {
+        imap_expunge($this->imap);
+        imap_close($this->imap);
     }
 }
-//getMailboxNames($imap, $server, $mailbox);
 
-$chats = imap_search($imap, 'ALL', SE_UID);
-
- foreach($chats as $key => $value) {
-     //$structure = imap_fetchstructure($imap, $value);
-     //echo "--------------<br />".$structure."<br />";
-     $headers = imap_headerinfo($imap, $value);
-     if(isset($headers->subject) && stristr(strtolower($headers->subject), $needle) == true) {
-        $body = imap_fetchbody($imap, $value, "2");
-        echo "--------------<br />".$body."<br />";
-     }
- }
-
-//for($m = 1; $m <= $message_count; $ms++) {
-//for($m = 1; $m <= 10; $m++) {
-//    $headers = imap_headerinfo($imap, $m);
-//    if(isset($headers->subject) && stristr(strtolower($headers->subject), $needle) == true) {
-    //if(isset($headers->subject)) {
-        //var_dump($headers);
-        //echo $headers->subject."<br />";
-//    }
-//
-//}
-
-// $jesse_chats = imap_search($imap, 'SUBJECT Jesse', SE_UID);
-
-// echo imap_fetchbody($imap, 3442, "1.1");
-
-// var_dump($jesse_chats);
-
-// foreach($jesse_chats as $key => $value) {
-//     $body = imap_fetchbody($imap, $value, "1");
-//     echo "--------------<br />".$body."<br />";
-// }
-
-        //var_dump($body);
-        //$header = imap_header($imap, $i);
-       
-        //DEBUGGING ONLY
-        //print_r($header);
-
-        /*foreach($jesse_chats as $key => $value) {
-
-        }
-        $body = imap_fetchbody($imap, $i, "1.1");
-       
-        if ($body == ""):
-            $body = imap_fetchbody($imap, $i, "1");
-        endif;
-       
-        $body = trim(substr(quoted_printable_decode($body), 0, 100));
-       
-        //GET THE VARIABLES WE NEED
-       
-        $email[$i]['from'] = $header->from[0]->mailbox.'@'.$header->from[0]->host;
-        $email[$i]['fromaddress'] = $header->from[0]->personal;
-        $email[$i]['to'] = $header->to[0]->mailbox;
-        $email[$i]['subject'] = $header->subject;
-        $email[$i]['reply_to'] = $header->reply_to[0]->mailbox.'@'.$header->reply_to[0]->host;
-        $email[$i]['message_id'] = $header->message_id;
-        $email[$i]['date'] = $header->udate;
-        $email[$i]['body'] = $body;
-       
-        $from = $email[$i]['fromaddress'];
-        $from_email = $email[$i]['from'];
-               
-                //DO WHATEVER YOU NEED TO PROCESS THE MESSAGE HERE
-                //ADVUST THIS FOR YOUR OWN USAGE --$message->save() probably wont mean anything to you
-                if($message->save()):
-           
-            imap_delete($imap, $i);
-           
-        endif;
- */
-    imap_expunge($imap);
-    imap_close($imap);
+$email = new Email(USER,PASSWORD);
+$email->insertChats();
+$email->closeImapConnection();
