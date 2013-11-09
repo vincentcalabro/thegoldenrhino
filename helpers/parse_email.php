8a1b53afee0784e47f508037b5b4fcee77e4d310
Vincent Calabro <vinny@1stdibs.com>
2013-11-09 02:07:03 -0500

    cleaning up email body, and other things

--- helpers/parse_email.php
+++ helpers/parse_email.php
@@ -4,6 +4,8 @@ require_once('/Users/vinny/projects/thegoldenrhino/setup/configuration.php');
 class Email {
     private $server = "{imap.gmail.com:993/ssl}";
     private $mailbox = "[Gmail]/Chats";
+    private $user;
+    Private $password;
     private $imap;
     private $db;
 
@@ -13,7 +15,7 @@ class Email {
         $this->imap = imap_open($this->server.$this->mailbox, $this->user, $this->password);  // get imap connection
     }
 
-    public function connectDB() {
+    public static function connectDB() {
         $db = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD);
         return $db;
     }
@@ -35,36 +37,37 @@ class Email {
         $chats_arr = array();
 
         $needle = "chat with jesse goodenough";     // search for this subject
-        $to_blank = array("display:block","padding-left:6em","text-indent:-1em",";");   // styles to replace with empty string
+        $to_blank = array("display:block;","padding-left:6em","text-indent:-1em","color:#888","&nbsp;");   // styles to replace with empty string
 
         foreach($chats as $value) {
-            $headers = imap_headerinfo($imap, $value);
-            if(isset($headers->subject) && stristr(strtolower($headers->subject), $needle) == true) {
-                $body = imap_fetchbody($imap, $value, "2");
-                $body = str_replace($to_blank,"",$body);
-                $body = preg_replace("/(0?\d|1[0-2]):(0\d|[0-5]\d) (AM|PM)/i","<span style='color:#777;'>$1:$2 $3</span>",$body);
-                $body = "<div class='conversation'>".$body."</div>";
-                array_push($chats_arr, $body);
+            if ($headers = imap_headerinfo($imap, $value)) {
+                if (isset($headers->subject) && stristr(strtolower($headers->subject), $needle) == true) {
+                    $body = imap_fetchbody($imap, $value, "2");
+                    $body = str_replace($to_blank,"",$body);
+                    $body = preg_replace("/(0?\d|1[0-2]):(0\d|[0-5]\d) (AM|PM)/i","<span style='color:#777;padding-right:5px;'>$1:$2 $3</span>",$body);
+                    //$body = str_replace("style=float:left;color:#888","style='color:#777;padding-right:5px;'",$body);
+                    $body = "<div class='conversation'>".$body."</div>";
+                    array_push($chats_arr, $body);
+                }
             }
         }
         return $chats_arr;
     }
 
     public function insertChats() {
-        $db = $this->connectDB();
+        $db = self::connectDB();
         $chats_arr = $this->getChatsFromJesse();
         foreach($chats_arr as $value) {
-            $sql = "insert into email.conversations (body) values ('".$value."')";
-            mysql_query($sql,$db);
+            $sql = 'insert into email.chats (body) values ("'.mysql_real_escape_string($value).'")';
+            mysql_query($sql,$db) or die(mysql_error());
         }
     }
 
     public function closeImapConnection() {
-        imap_expunge($this->imap);
         imap_close($this->imap);
     }
 }
 
-$email = new Email(USER,PASSWORD);
-$email->insertChats();
-$email->closeImapConnection();
+//$email = new Email(USER,PASSWORD);
+//$email->insertChats();
+//$email->closeImapConnection();
